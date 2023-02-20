ca7f46ef7c6d2eee7ca54dde172a7b4b0894a43d
StellaCentis <stella.centis@gmail.com>
2023-02-20 21:36:32 +0100

    Description of the function

--- new_functions.py
+++ new_functions.py
@@ -1,4 +1,9 @@
 def first_word(string):
+     '''
+    This function returns the first word of a string
+    input: string
+    output: first word of the input string
+    '''
     all_words = string.split()
     first_word= all_words[0]
     return first_word
