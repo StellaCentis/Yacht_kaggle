3628dc3b1148dcc3dcef2f6c13f91f8fd4743ece
StellaCentis <stella.centis@gmail.com>
2023-02-20 21:56:35 +0100

    Function bettered

--- new_functions.py
+++ new_functions.py
@@ -1,9 +1,10 @@
-def first_word(string):
-     '''
+def first_word(a_string):
+    '''
     This function returns the first word of a string
-    input: string
+    input: a_string
     output: first word of the input string
     '''
-    all_words = string.split()
-    first_word= all_words[0]
-    return first_word
+    all_words = a_string.split()
+    my_word= all_words[0]
+    return my_word
+    
